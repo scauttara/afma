33a1e09e31848544d15d9a153632a2dfc5ee2397
Md Asad Chowdhary <artsvasad@gmail.com>
2025-12-18 18:38:56 +0600

    update server settings

--- server/index.js
+++ server/index.js
@@ -2,15 +2,17 @@ import express from 'express';
 import mongoose from 'mongoose';
 import cors from 'cors';
 import marksRoutes from './routes/marksRoutes.js'; // Note the .js extension
-
+import dotenv from 'dotenv';
 const app = express();
+dotenv.config()
 
+const MONGO_URI = process.env.MONGO_URI
 // Middleware
 app.use(cors());
 app.use(express.json({ limit: '50mb' }));
 
 // Database Connection
-mongoose.connect('mongodb://localhost:27017/afma')
+mongoose.connect(MONGO_URI)
   .then(() => console.log('MongoDB Connected'))
   .catch(err => console.error('DB Error:', err));
 
